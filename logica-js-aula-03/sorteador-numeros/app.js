2f243cdfe5869b32d6ae974f1cc5194d07f2dac5
Junior Facin <jufacin@hotmail.com>
2024-02-21 08:24:59 -0300

    Beginning the 3rd Logic course: practical challenges

---
+++ logica-js-aula-03/sorteador-numeros/app.js
@@ -0,0 +1,9 @@
+function sortear() {
+  let quantidade = parseInt(document.getElementById('quantidade').value);
+  let de = parseInt(document.getElementById('de').value);
+  let ate = parseInt(document.getElementById('ate').value);
+
+  alert(`Quantidade: ${quantidade}`);
+  alert(`De: ${de}`);
+  alert(`Ate: ${ate}`);
+}
